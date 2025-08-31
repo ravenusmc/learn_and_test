e12e2433e7fae0f707cb82135e2e931de9071677
ravenusmc <mcuddy77@gmail.com>
2025-08-31 13:29:34 -0400

    finished first program

---
+++ 57_exercises/one.py
@@ -0,0 +1,5 @@
+# Create a program where the user enters their name and it greats them. 
+
+first_name = input("Please enter your first name: ")
+last_name = input("Pleaes enter your last name: ")
+print('Hello ' + first_name + ' ' + last_name + ' nice to meet you!')
