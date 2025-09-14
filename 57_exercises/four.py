c3f31142e583229ae827f0b7bb4d8c232e3ef4fb
ravenusmc <mcuddy77@gmail.com>
2025-09-14 13:34:30 -0400

    started worked on first method

--- 57_exercises/four.py
+++ 57_exercises/four.py
@@ -1,2 +1,7 @@
 # Fourth Exercise - Simple math problem
 # Take two numbers and show all mathematical operations on them
+
+def program_start():
+  answer = input('Would you like to use a simple program? (y/n) ')
+
+program_start()
