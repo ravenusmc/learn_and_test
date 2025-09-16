989e6f8834ce934cf8b53e625e007cf04abe17c5
ravenusmc <mcuddy77@gmail.com>
2025-09-16 16:55:06 -0400

    added a method to get second number

--- 57_exercises/four.py
+++ 57_exercises/four.py
@@ -2,6 +2,33 @@
 # Take two numbers and show all mathematical operations on them
 
 def program_start():
-  answer = input('Would you like to use a simple program? (y/n) ')
+  answer = input('Would you like to use a simple program? (y/n) ').lower()
+  if answer == 'n':
+    print('Thank you! Have a great day!')
+  else:
+    first_number = get_first_number()
+
+def get_first_number():
+    while True:
+      first_number = input("Enter a number: ")
+      result = number_check(first_number)
+      if result is not None:  # valid number
+          return result  # exit loop and return number
+
+def get_second_number():
+  while True:
+    second_number = input("Enter a number: ")
+    result = number_check(second_number)
+    if result is not None:  # valid number
+        return result  # exit loop and return number
+
+def number_check(number):
+    try:
+        return int(number)
+    except ValueError:
+        print("Please enter a valid number.")
+        return None
+    
+
 
 program_start()
