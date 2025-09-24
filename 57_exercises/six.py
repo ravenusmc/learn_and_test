39d214bea4851809123fe8fa348c5a80ca4300f2
ravenusmc <mcuddy77@gmail.com>
2025-09-24 16:20:00 -0400

    added a new method to sixth program

--- 57_exercises/six.py
+++ 57_exercises/six.py
@@ -6,9 +6,28 @@ def program_start():
   if answer == 'n':
     print("Thank you for using!")
   else: 
-    get_length_of_room()
+    length_of_room = get_length_of_room()
+    width_of_room = get_width_of_room()
 
 def get_length_of_room():
-  length_of_room = input("What is the length of your room: ")
+  while True: 
+    length_of_room = input("What is the length of the room in feet: ")
+    result = number_check(length_of_room)
+    if result is not None:  # valid number
+        return result  # exit loop and return number
 
+def get_width_of_room():
+  while True: 
+    width_of_room = input('What is the width of the room in feet: ')
+    result = number_check(width_of_room)
+    if result is not None:  # valid number
+        return result  # exit loop and return number
+
+def number_check(number):
+    try:
+        return int(number)
+    except ValueError:
+        print("Please enter a valid number.")
+        return None
+    
 program_start()
