dd9bc9c93a1da9bbb9927f8f3d4aa163045e24a5
ravenusmc <mcuddy77@gmail.com>
2025-09-30 17:51:51 -0400

    started work on program

--- 57_exercises/seven.py
+++ 57_exercises/seven.py
@@ -1,1 +1,11 @@
-# Seventh program 
+# Seventh program - Pizza Party - write a program to evenly divide pizza's. prompt for the number of people
+# The number of pizza's and the number of slices per pizza. Ensure that the number turns out even. Display
+# the number of pieces of pizze each person should get. If there are left overs show the number of leftovers.
+# How many people 
+# How many pizzas do you have 
+
+def program_start():
+  pass
+
+
+program_start()
