a4df49090fef413006a0934d1ac447a3da05e061
ravenusmc <mcuddy77@gmail.com>
2025-11-13 16:33:17 -0500

    started work on reports project

---
+++ reports_project/main.py
@@ -0,0 +1,11 @@
+# This project will focus on how to take csv files, filter them for data and then bring 
+# them back together to make one excel file. 
+
+class Reports():
+
+  def start_program(self):
+    print('hello')
+  
+
+obj_start = Reports()
+obj_start.start_program()
