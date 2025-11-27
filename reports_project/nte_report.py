7dd38077a7c76ee3e58d72d725fbce5e24ea4589
ravenusmc <mcuddy77@gmail.com>
2025-11-27 12:45:40 -0500

    minor change

--- reports_project/nte_report.py
+++ reports_project/nte_report.py
@@ -1,5 +1,6 @@
 # This project will focus on how to take csv files, filter them for data and then bring 
 # them back together to make one excel file. 
+# I'll most likely be making this project on my work computer. 
 
 # importing supporting libraries
 import pandas as pd
