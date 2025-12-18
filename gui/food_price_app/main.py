7f86d7e5bffdf40dff92ec8b447a182234408497
ravenusmc <mcuddy77@gmail.com>
2025-12-18 17:02:48 -0500

    added in radio buttons that work

--- gui/food_price_app/main.py
+++ gui/food_price_app/main.py
@@ -1,4 +1,4 @@
-# This is the first file learning the basics of tkinter. 
+# This is the first file learning the basics of tkinter. It will be a basic food price app. 
 
 from tkinter import *
 
@@ -9,16 +9,18 @@ root.resizable(width=False, height=False)
 color = 'gray77'
 root.configure(bg=color)
 
+v = StringVar()
+
 label = Label(root, text='Choose An Item', bg=color)
 label.place(x=100, y=5)
 
-r_btn = Radiobutton(root, text='Banana', bg=color)
+r_btn = Radiobutton(root, text='Banana', bg=color, variable=v, value='Banana')
 r_btn.place(x=5, y=30)
 
-r_btn2 = Radiobutton(root, text='Apple', bg=color)
+r_btn2 = Radiobutton(root, text='Apple', bg=color, variable=v, value='Apple')
 r_btn2.place(x=5, y=60)
 
-r_btn3 = Radiobutton(root, text='Orange', bg=color)
+r_btn3 = Radiobutton(root, text='Orange', bg=color, variable=v, value='Orange')
 r_btn3.place(x=5, y=90)
 
 entry = Entry(root, width=20)
