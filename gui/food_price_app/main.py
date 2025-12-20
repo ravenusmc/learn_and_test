bfad944028be54771b9ddb8b6ce0c47a64587986
ravenusmc <mcuddy77@gmail.com>
2025-12-20 11:36:25 -0500

    fixed issue with radio buttons

--- gui/food_price_app/main.py
+++ gui/food_price_app/main.py
@@ -10,18 +10,19 @@ root.resizable(width=False, height=False)
 color = 'gray77'
 root.configure(bg=color)
 
-v = StringVar()
+v = IntVar()
+# v = StringVar()
 
 label = Label(root, text='Choose An Item', bg=color)
 label.place(x=100, y=5)
 
-r_btn = Radiobutton(root, text='Banana', bg=color, variable=v, value='Banana')
+r_btn = Radiobutton(root, text='Banana', bg=color, variable=v, value=0)
 r_btn.place(x=5, y=30)
 
-r_btn2 = Radiobutton(root, text='Apple', bg=color, variable=v, value='Apple')
+r_btn2 = Radiobutton(root, text='Apple', bg=color, variable=v, value=1)
 r_btn2.place(x=5, y=60)
 
-r_btn3 = Radiobutton(root, text='Orange', bg=color, variable=v, value='Orange')
+r_btn3 = Radiobutton(root, text='Orange', bg=color, variable=v, value=2)
 r_btn3.place(x=5, y=90)
 
 entry = Entry(root, width=20)
