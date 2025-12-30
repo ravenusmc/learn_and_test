41ec462ad07861c03b4745d1460c98b63b7c0771
ravenusmc <mcuddy77@gmail.com>
2025-12-30 16:27:52 -0500

    finished class section

--- gui/classes/main.py
+++ gui/classes/main.py
@@ -10,8 +10,16 @@ class MyFirstUI:
     self.master.configure(bg=self.master.color)
     self.label = Label(master, text='This is label')
     self.label.pack()
+    self.btn = Button(master, text='Click Me', command=self.btn_action)
+    self.btn.pack()
+    self.btn2 = Button(master, text='Quit', command=self.quit)
+    self.btn2.pack()
 
+  def btn_action(self):
+    print('This button is clicked')
 
+  def quit(self):
+    self.master.quit()
 
 
 root = Tk() 
