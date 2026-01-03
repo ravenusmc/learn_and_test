fb9101a3054b4a320bccadb97f9264a1434347e1
ravenusmc <mcuddy77@gmail.com>
2026-01-03 15:10:58 -0500

    worked on events lesson

--- gui/basics/events.py
+++ gui/basics/events.py
@@ -2,5 +2,20 @@ from tkinter import *
 
 root = Tk() 
 
+def key(event):
+    print("pressed", event.char)
+
+def callback(event):
+    frame.focus_set()
+    print("Eevent x", event.x, "Event y", event.y)
+
+frame = Frame(root, width=100, height=100)
+frame.bind("<Key>", key)
+frame.bind("<Button-1>", callback)
+frame.pack()
+
+
+
+
 
 root.mainloop() 
