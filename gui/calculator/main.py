fbe997ac2f915eb5304fcc3fd4ff303f315ec7f7
ravenusmc <mcuddy77@gmail.com>
2026-01-05 16:36:43 -0500

    added buttons to calculator

--- gui/calculator/main.py
+++ gui/calculator/main.py
@@ -8,6 +8,7 @@ root.geometry('400x150')
 root.title('Calculator')
 color='gray'
 root.configure(bg=color)
+root.resizable(width=False, height=False)
 
 # ---------- Frames 
 
@@ -17,11 +18,25 @@ top_first.pack(side=TOP)
 top_second = Frame(root, width=800, height=40, bg='black')
 top_second.pack(side=TOP)
 
-top_third = Frame(root, width=800, height=40, bg='blue')
+top_third = Frame(root, width=800, height=40, bg=color)
 top_third.pack(side=TOP)
 
 top_fourth = Frame(root, width=800, height=40, bg='white')
 top_fourth.pack(side=TOP)
+
+# ---------- Button 
+
+btn_plus = Button(top_third, text='+', width=6, highlightbackground=color)
+btn_plus.pack(side=LEFT, padx=3, pady=3)
+
+btn_minus = Button(top_third, text='-', width=6, highlightbackground=color)
+btn_minus.pack(side=LEFT, padx=3, pady=3)
+
+btn_mul = Button(top_third, text='*', width=6, highlightbackground=color)
+btn_mul.pack(side=LEFT, padx=3, pady=3)
+
+btn_div = Button(top_third, text='/', width=6, highlightbackground=color)
+btn_div.pack(side=LEFT, padx=3, pady=3)
 
 
 
