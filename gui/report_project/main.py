4426907a6ec4196297edbb8e793f7e403a026b24
ravenusmc <mcuddy77@gmail.com>
2026-01-16 18:48:03 -0500

    minor changed

--- gui/report_project/main.py
+++ gui/report_project/main.py
@@ -1,12 +1,25 @@
 # This is a test program for an application that I want to build for my work. 
 
+# importing supporting libraries for project
 from tkinter import * 
 from tkinter import filedialog
+import pandas as pd
+import os
+import json
+import glob
 
 class ReportGui:
   
   def __init__(self, master):
-    self.master = master 
+    self.master = master
+    self.monthly_report = pd.read_excel(
+        './new_month_report/new_report_1.xlsx',
+        sheet_name=None,
+        engine='openpyxl'
+    ) 
+    self.CLP_DF = self.monthly_report["CLP"]
+    self.EXC_DF = self.monthly_report["EXC"]
+    self.WIGI_DF = self.monthly_report["WIGI"]
     self.master.title('Report Makr')
     self.master.geometry('400x400')
     self.master.color = 'gray55'
