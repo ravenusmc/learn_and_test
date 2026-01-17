2430b99299f52e4e60ca96a6b0658f13d281d00b
ravenusmc <mcuddy77@gmail.com>
2026-01-17 11:19:45 -0500

    working on reports gui

--- gui/report_project/main.py
+++ gui/report_project/main.py
@@ -12,23 +12,34 @@ class ReportGui:
   
   def __init__(self, master):
     self.master = master
-    self.monthly_report = pd.read_excel(
-        './new_month_report/new_report_1.xlsx',
-        sheet_name=None,
-        engine='openpyxl'
-    ) 
-    self.CLP_DF = self.monthly_report["CLP"]
-    self.EXC_DF = self.monthly_report["EXC"]
-    self.WIGI_DF = self.monthly_report["WIGI"]
+    self.monthly_report = ""
+    self.CLP_DF = ""
+    self.EXC_DF = ""
+    self.WIGI_DF = ""
     self.master.title('Report Makr')
     self.master.geometry('400x400')
     self.master.color = 'gray55'
     self.master.configure(bg=self.master.color)
     self.btn = Button(master, text='Select New Report', command=self.get_new_monthly_report)
     self.btn.pack()
+    self.label = Label(master, text='This is label')
+    self.label.pack()
   
   def get_new_monthly_report(self):
-    root.filename = filedialog.askopenfilename()
+    file_path = filedialog.askopenfilename(
+        title="Select Monthly Report",
+        filetypes=[("Excel files", "*.xlsx *.xls")]
+    )
+    if not file_path:
+        return  # user cancelled
+    self.monthly_report = pd.read_excel(
+        file_path,
+        sheet_name=None,
+        engine='openpyxl'
+    )
+    self.CLP_DF = self.monthly_report["CLP"]
+    self.EXC_DF = self.monthly_report["EXC"]
+    self.WIGI_DF = self.monthly_report["WIGI"]
   
   def break_up_new_monthly_report(self):
     pass
