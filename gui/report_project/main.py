6449763273c31394adb83c4557fa934e04a08a6a
ravenusmc <mcuddy77@gmail.com>
2026-01-21 16:58:13 -0500

    working on filtering clp file

--- gui/report_project/main.py
+++ gui/report_project/main.py
@@ -20,14 +20,19 @@ class ReportGui:
     self.master.geometry('400x300')
     self.master.color = 'gray55'
     self.master.configure(bg=self.master.color)
+    # Office codes JSON set up 
+    self.office_code_map = self.load_office_codes()
     #Reports Parameters 
     self.monthly_report = ""
     self.CLP_DF = ""
     self.EXC_DF = ""
     self.WIGI_DF = ""
+    # These are the CSV files 
     self.CLP_OLD = ""
     self.EXC_OLD = ""
     self.WIGI_OLD = ""
+    # These are the sorted and cleaned CSV files 
+    self.CLP_Filtered_Old = ""
     self.office = ""
     self.offices = ['ABC', 'DEF', 'GHI']
     self.office_var = StringVar()
@@ -42,12 +47,13 @@ class ReportGui:
     row = Frame(master, bg=self.master.color)
     row.pack(pady=10)
     Label(row, text="Office:", bg=self.master.color).pack(side=LEFT, padx=(0, 5))
-    self.office_entry = Entry(
+    self.office_menu = OptionMenu(
       row,
-      highlightbackground=self.master.color,
-      textvariable=self.office_var
+      self.office_var,
+      *self.offices
     )
-    self.office_entry.pack(side=LEFT)
+    self.office_menu.config(bg=self.master.color)
+    self.office_menu.pack(side=LEFT)
 
     # Entry Month Code: 
     row_2 = Frame(master, bg=self.master.color)
@@ -61,12 +67,13 @@ class ReportGui:
     self.month_menu.config(bg=self.master.color)
     self.month_menu.pack(side=LEFT)
 
-    # self.office_entry.pack()
+    # Buttons 
     self.btn = Button(master, text='Select New Report', command=self.get_new_monthly_report)
     self.btn.pack()
     self.btn = Button(master, text='Select Old Monthly Report', command=self.get_new_monthly_report)
     self.btn.pack()
-
+    self.btn = Button(master, text='Execute Program', command=self.build_new_report)
+    self.btn.pack()
   
   def get_new_monthly_report(self):
     self.office = self.office_var.get().strip()
@@ -77,7 +84,7 @@ class ReportGui:
     if not self.month:
       messagebox.showerror("Missing Month", "Please enter a month")
       return
-    print("Office entered:", self.month) 
+    # print("Office entered:", self.office) 
     file_path = filedialog.askopenfilename(
         title="Select Monthly Report",
         filetypes=[("Excel files", "*.xlsx *.xls")]
@@ -110,17 +117,34 @@ class ReportGui:
         elif sheet_name == "WIGI":
             self.WIGI_OLD = buffer.getvalue()
   
+  def build_new_report(self):
+    self.filter_clps_by_office()
+  
   def filter_clps_by_office(self):
-    clp_path = os.path.join(self.csv_folder, "CLP.csv")
-    df = pd.read_csv(clp_path, dtype=str)
+    df = pd.read_csv(StringIO(self.CLP_OLD))
     df.columns = df.columns.str.strip().str.upper()
     org_field = "ORG CODE"
     sort_field = "ENTER PRES GR"
-    df_filtered = df[df[org_field].isin(self.office_codes[self.selected_office])]
+    df_filtered = df[df[org_field].isin(self.office_codes[self.office])]
     df_filtered = df_filtered.sort_values(by=sort_field, ascending=True)
-    office = self.selected_office
-    csv_path = os.path.join(self.csv_folder, f"{office}_CLP.csv")
-    df_filtered.to_csv(csv_path, index=False)
+    # office = self.office
+    buffer = StringIO()
+    df_filtered.to_csv(buffer, index=False)
+    buffer.seek(0)
+    self.CLP_Filtered_Old = buffer.getvalue()
+    print(self.CLP_Filtered_Old.splitlines()[:5])
+
+    # OLD CODE 
+    # clp_path = os.path.join(self.csv_folder, "CLP.csv")
+    # df = pd.read_csv(clp_path, dtype=str)
+    # df.columns = df.columns.str.strip().str.upper()
+    # org_field = "ORG CODE"
+    # sort_field = "ENTER PRES GR"
+    # df_filtered = df[df[org_field].isin(self.office_codes[self.selected_office])]
+    # df_filtered = df_filtered.sort_values(by=sort_field, ascending=True)
+    # office = self.selected_office
+    # csv_path = os.path.join(self.csv_folder, f"{office}_CLP.csv")
+    # df_filtered.to_csv(csv_path, index=False)
 
   
   def break_up_new_monthly_report(self):
